a4135c9218f49b06e777205a14627f53240cbe79
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-05-01 21:54:20 +0300

    Add files via upload

--- Web Scraper.py
+++ Web Scraper.py
@@ -16,21 +16,21 @@ def web_scraper(url):
     h2=[item.text for item in soup.find_all("h2")]
     h3 = [item.text for item in soup.find_all("h3")]
     p = [item.text for item in soup.find_all("p")]
-    scraped_content=[url,title,h1,h2,h3,p]
+    a = [item.text for item in soup.find_all("a")]
+    div = [item.text for item in soup.find_all("div")]
+    scraped_content=[url,title,h1,h2,h3,p,a,div]
     return scraped_content
 
 # Scraping a list of URLs and storing them in a DataFrame
 
-urls=["https://www.allot.com",
-      "https://www.investopedia.com/peloton-s-risky-business-model-4768768",
-      "https://www.investopedia.com/articles/exchangetradedfunds/11/advantages-disadvantages-etfs.asp",
-      "https://www.investopedia.com/terms/c/comparative-market-analysis.asp",
-      "https://www.investopedia.com/purchasing-a-home-4689702"]
+urls=["https://www.zap.co.il/models.aspx?sog=c-tabletpc",
+      "https://www.zap.co.il/models.aspx?sog=c-tabletpc&db53548=4255448"]
+
 all_results=[]
 for url in urls:
     X=web_scraper(url)
     all_results.append(X)
-df = DataFrame(all_results,columns=["URL","Title","H1","H2","H3","p"])
+df = DataFrame(all_results,columns=["URL","Title","H1","H2","H3","p","a","div"])
 
 # export to excel
 
