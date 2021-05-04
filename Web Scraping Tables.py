eb47c563442b33315445e81e03ae712950d8a787
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-05-04 15:20:27 +0300

    Add files via upload

---
+++ Web Scraping Tables.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+
+# Webpage url
+url = 'https://www.bls.gov/oes/current/oes_nat.htm'
+
+# Extract tables
+dfs = pd.read_html(url)
+
+# Get first table
+df = dfs[0]
+
+# Extract columns
+df2 = df[['Occupation code','Level']]
+print(df2)
+
+df.to_excel("scraped_output.xlsx")
