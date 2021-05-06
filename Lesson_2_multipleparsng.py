84fb10603383f71592b3a5a4c6c1216aaacb6054
dinspi3 <47669142+dinspi3@users.noreply.github.com>
2021-05-06 22:39:26 +0300

    Add files via upload

--- Lesson_2_multipleparsng.py
+++ Lesson_2_multipleparsng.py
@@ -5,10 +5,22 @@ def get_html(url):
     r = requests.get(url)
     return r.text
 
+def refined(s):
+    r=s.split(' ')[0]
+    print(r)
+
 def get_data(html):
     soup = BeautifulSoup(html,'lxml')
     popular = soup.find_all('section')[1]
-    return popular
+    plugins = popular.find_all('article')
+    #return len(plugins)
+
+    for plugin in plugins :
+        name = plugin.find('h3').find('a').get('href')
+        rating = plugin.find('span',class_='rating-count').find('a').text
+        #print(rating)
+
+        refined(rating)
 
 
 def main():
